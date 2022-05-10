caef848d054a570f9bb208868dd68ea35e07e925
danielpdwalker <danielpdwalker@gmail.com>
2022-05-10 14:54:25 +0100

    Fixed boto3 dependency issue by pinning version. Upgraded snowflake-connector-python and inflection versions. Bumped package version to 0.1.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="target-snowflake",
-    version="0.1.3",
+    version="0.1.4",
     author="Meltano",
     author_email="meltano@gitlab.com",
     description="Singer.io target for importing data to Snowflake",
@@ -16,15 +16,15 @@ setup(
     url="https://gitlab.com/meltano/target-snowflake",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     install_requires=[
-        'inflection==0.3.1',
+        'inflection==0.5.1',
         'singer-python==5.2.3',
-        'asn1crypto<1.0.0',
         # conflicts resolution, see https://github.com/snowflakedb/snowflake-connector-python/issues/225
         'azure-storage-blob<12.0',
-        'snowflake-connector-python==1.7.11',
+        'snowflake-connector-python==2.7.*',
         'snowflake-sqlalchemy==1.3.3',
         # conflicts resolution, see https://gitlab.com/meltano/meltano/issues/193
         'idna==2.7',
+        'boto3==1.21',
     ],
     extras_require={
         'dev': [
